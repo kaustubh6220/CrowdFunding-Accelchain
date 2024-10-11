bf5265d01c73545f0aadd80bc63c09f5a9086e4a
kaustubh mane <kaustubh6220@gmail.com>
2024-10-11 16:30:26 +0530

    UI change 2.0

--- frontend/app/campagins/[creator]/[campaginId]/page.tsx
+++ frontend/app/campagins/[creator]/[campaginId]/page.tsx
@@ -126,8 +126,8 @@ const CampaignDetails = ({ params: { creator, campaginId } }: CampaignId) => {
                         </CardTitle>
                         <FaEthereum className="h-4 w-4 text-muted-foreground" />
                     </CardHeader>
-                    <CardContent>
-                        <div className="text-2xl font-bold">{remainingAmount} Eth</div>
+                    <CardContent className=''>
+                        <div className="text-2xl font-bold line-clamp-1">{remainingAmount} Eth</div>
                         <p className="text-xs text-muted-foreground">
                         remaining
                         </p>
